107fad6921fbde8976d5aad80ff1d596dad7682c
Angga Dwi Arifandi <namapanjanggua@gmail.com>
2020-05-08 07:54:20 +0700

    fix filter

--- src/context/TransactionsContext.js
+++ src/context/TransactionsContext.js
@@ -42,11 +42,7 @@ const TransactionsReducer = (state, action) => {
         return state; // we don't want to search if data haven't been fetched yet
       }
       const query = action.payload.query;
-      const source =
-        state.filteredTransactions == null
-          ? state.transactions
-          : state.filteredTransactions;
-      const result = source.filter(item => {
+      const result = state.transactions.filter(item => {
         const bool = Utils.searchForOccurence({
           criterias: [
             item.beneficiary_name,
